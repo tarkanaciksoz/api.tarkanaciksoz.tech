3dd896f1dbe96f12fc3a0c51e17ed23cec981da1
tarkanaciksoz <tarkanaciksoz@gmail.com>
2021-10-22 18:07:40 +0300

    cachefix

--- api.go
+++ api.go
@@ -72,6 +72,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	response := setAndGetResponse(true, "başarılıydı", respon, 200).([]byte)
 
 	fmt.Fprint(w, string(response))
+	return
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
@@ -99,6 +100,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	response := setAndGetResponse(true, "Başarılı.", cData, 200).([]byte)
 
 	fmt.Fprint(w, string(response))
+	return
 }
 
 /*func (r Request) getWriter() http.ResponseWriter {
